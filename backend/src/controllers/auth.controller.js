f093f975e0946c0c991726ed367d7f68fd381297
MohammadSameer2785 <mohammad.sameer.9271@gmail.com>
2026-06-27 20:56:46 +0530

    Implemented the email functionality using resend with a beautiful email template

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
 
@@ -49,7 +49,11 @@ export const signup = async (req, res) => {
         email: newUser.email,
         profilePic: newUser.profilePic,
       });
-
+       try {
+        await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+      } catch (error) {
+        console.error("Failed to send welcome email:", error);
+      }
     } else {
       res.status(400).json({ message: "Invalid user data" });
     }
