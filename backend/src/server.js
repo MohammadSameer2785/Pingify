c4e6232cc57580d900e2e33bc1d40b7f6f545e1c
MohammadSameer2785 <mohammad.sameer.9271@gmail.com>
2026-06-27 16:41:19 +0530

    Make ready to deploy the application

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,22 @@
 //const express=require('express'); common js
 import express from 'express' //ES module
 import dotenv from "dotenv"
+import path from "path"
+
 import authRoutes from "./routes/auth.route.js"
 const app=express();
+const __dirname=path.resolve();
 dotenv.config();
 const PORT=process.env.PORT||3000;
 app.use("/api/auth",authRoutes)
+
+//make ready to deployment 
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+    })
+}
+
 app.listen(PORT,()=>{console.log(`app is listening on the port ${PORT}`)})
