0d474863e6a864ee82525ffd7c5f41706ef40448
MohammadSameer2785 <mohammad.sameer.9271@gmail.com>
2026-06-27 21:13:33 +0530

    Implemented the login,logout API'S

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup,login,logout } from "../controllers/auth.controller.js";
 const router=express.Router();
 
-router.get("/login",(req,res)=>{
-    res.send("login")
-})
+router.get("/login",login)
 router.post("/signup",signup)
+router.post("/logout",logout)
 
 export default router;
