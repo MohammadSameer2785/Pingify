3d0789bf60b74c6b317a13b9a30f4e86f4883a14
MohammadSameer2785 <mohammad.sameer.9271@gmail.com>
2026-07-21 16:57:23 +0530

    Remove frontend static serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,34 +1,36 @@
-//const express=require('express'); common js
-import express from 'express' //ES module
-import dotenv from "dotenv"
-import path from "path"
+// const express = require("express"); // CommonJS
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-import {connectDB}  from './lib/db.js'
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-const __dirname=path.resolve();
+
 dotenv.config();
-const PORT=process.env.PORT||3000;
-app.use(cookieParser()); 
-app.use(cors({
-  origin: process.env.CLIENT_URL || "*",
-  credentials: true
-}));
-app.use(express.json({limit:"5mb"}));//It is used get the fields that are entered in the frontend (body)
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-//make ready to deployment 
-if(process.env.NODE_ENV=="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    
-    app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
-    })
-}
+const PORT = process.env.PORT || 3000;
 
-server.listen(PORT,()=>{
-    connectDB();
-    console.log(`app is listening on the port ${PORT}`)})
+// Middlewares
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "5mb" }));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+// Start Server
+server.listen(PORT, () => {
+  connectDB();
+  console.log(`App is listening on port ${PORT}`);
+});
