3af772a350e9d6838f35ae952832f714583c7340
MohammadSameer2785 <mohammad.sameer.9271@gmail.com>
2026-07-21 17:27:41 +0530

    Debug CLIENT_URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-
+console.log("CLIENT_URL =", process.env.CLIENT_URL);
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
